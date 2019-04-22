38c7ee9ac43e9951f1533715fc789bcc98ee7b9c
guanjiajia <guanjiajia@linqsoft.com>
2019-04-22 18:48:55 +0800

    :Add New Icon

---
+++ Assets/Scripts/Common/SpriteAnimation.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SpriteAnimation : MonoBehaviour {
+
+	public Sprite[] m_Sprites;
+	public float timeDuration = 0.1f;
+
+	// Use this for initialization
+	void OnEnable () {
+		StartCoroutine(play());
+	}
+
+	IEnumerator play()
+	{
+		int i = 0;
+
+		while(i < m_Sprites.Length)
+		{
+			transform.GetComponent<Image>().sprite = m_Sprites[i];
+			i++;
+			yield return new WaitForSeconds(timeDuration);
+
+			if (i >= m_Sprites.Length)
+				i = 0;
+		}
+	}
+
+}
