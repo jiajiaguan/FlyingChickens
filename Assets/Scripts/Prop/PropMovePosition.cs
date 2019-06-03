52ee21f5b481a501f8eea90f417d098ea746747b
guanjiajia <guanjiajia@linqsoft.com>
2019-06-03 09:11:17 +0800

    PropMovePosition

--- Assets/Scripts/Prop/PropMovePosition.cs
+++ Assets/Scripts/Prop/PropMovePosition.cs
@@ -25,7 +25,7 @@ public class PropMovePosition : MonoBehaviour {
         // transform.DOLocalMove(m_EndPos, m_Duration).SetLoops(-1,LoopType.Yoyo).SetDelay(m_DelayTime);
         // m_rigibody = transform.Getcomm
         m_EndPos = parent.TransformPoint(m_EndPos);
-        m_rigibody.DOMove(m_EndPos, m_Duration).SetLoops(-1,LoopType.Yoyo).SetDelay(m_DelayTime);
+        m_rigibody.DOMoveX(m_EndPos.x, m_Duration).SetLoops(-1,LoopType.Yoyo).SetDelay(m_DelayTime);
     }
 	
 	// Update is called once per frame
